1aac6907b3545130d798e9e45a605d8d4b984cbd
Igor Gabriel <igorgabsprofissional@gmail.com>
2024-05-07 11:08:28 -0300

    Added: Alterações no script e responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,40 @@
 const getAnswwers = document.getElementsByClassName('answers');
 const getMain = document.querySelector('main');
 
+localStorage.setItem('My Gifs', myGifs);
+
 for (const i of getAnswwers) {
     i.addEventListener('click', (e)=>{
+        
         let contentAnswers;
 
-        switch(i) {
+        switch(i.textContent) {
             case 'Feliz':
                 contentAnswers = 'happy';
             break;
                 
             case 'Triste':
-                contentAnswers = 'happy';
+                contentAnswers = 'sad';
             break;
                 
             case 'Apaixonado':
-                contentAnswers = 'hapy';
+                contentAnswers = 'love';
+            break;
+
+            case 'Nervoso':
+                contentAnswers = 'scared';
             break;
                 
-            case 'Nervoso':
-                contentAnswers = 'hapy';
+            case 'Sono':
+                contentAnswers = 'sleep';
+            break;
+
+            case 'Raiva':
+                contentAnswers = 'angrystare';
             break;
 
             default:
+                
                 break;
         }   
 
@@ -34,8 +46,22 @@ for (const i of getAnswwers) {
         
                 const responseJson = await response.json();
 
-                console.log(responseJson);
+                if (response.status === 200) {
 
+                    myGifs.push(responseJson);
+
+                    getMain.innerHTML = `
+                        <div id="show-gif">
+                            <img id="miku-show-gif" src="./assets/img/miku/miku-person-3.png">
+                            <img id="gif" src="${responseJson.url}">
+                        </div>
+                    `
+
+                } else {
+                    document.body.innerHTML = `
+                        
+                    `
+                }
                 
             } catch(error){
         
@@ -44,7 +70,9 @@ for (const i of getAnswwers) {
             }
             
         }
+
         requestWaifu();
+
     })
 };
 
