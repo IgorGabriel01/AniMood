d79ba0c52fd43ad3dad24005f2cc826cb5680783
Igor Gabriel <igorgabsprofissional@gmail.com>
2024-06-27 19:16:00 -0300

    Mudança na estruturação

---
+++ src/js/control-theme.js
@@ -0,0 +1,88 @@
+const iconsGifAndThemes = document.getElementsByClassName('div-header-options');
+const buttonDarkTheme = document.getElementById('dark-theme');
+const infosMiku = document.querySelector('main p');
+const question = document.getElementById('salutation-div');
+const answers = document.getElementsByClassName('answers');
+
+let controlsDarkTheme;
+
+function toPlaceThemeDark(){
+
+    if(!controlsDarkTheme){
+
+        document.body.style.backgroundColor = '#0B0B0B';
+        document.body.style.transition = '0.5s';
+
+        iconsGifAndThemes[0].innerHTML = `
+            <img class="icons-header" src="/assets/img/icons/gif-icon-light.png" alt="Ícone meus Gifs">
+            <p>Meus Gifs</p>
+        `;
+
+        iconsGifAndThemes[1].innerHTML = `
+            <img class="icons-header" src="/assets/img/icons/theme-light.png" alt="Ícone de tema claro">
+            <p>Light Mode</p>
+        `;
+
+        for (let i = 0; i < iconsGifAndThemes.length; i++) {
+            iconsGifAndThemes[i].style.color = '#F2F2F2'
+            iconsGifAndThemes[i].style.transition = '0.5s'
+        }
+
+        infosMiku.style.color = '#F2F2F2';
+        infosMiku.style.transition = '0.5s';
+
+        question.classList.add('answers-questions-black');
+
+        for (let i = 0; i < answers.length; i++) {
+            answers[i].classList.add('answers-questions-black');
+        }
+
+        if (document.getElementById('in-system-show-gifs')) {
+            document.getElementById('in-system-show-gifs').classList.toggle('answers-questions-black');
+        }
+
+        controlsDarkTheme = true;
+
+        localStorage.setItem('themeDark', true);
+
+    } else {
+
+        document.body.style.backgroundColor = '#ffffff';
+        document.body.style.transition = '0.5s';
+
+        iconsGifAndThemes[0].innerHTML = `
+            <img class="icons-header" src="/assets/img/icons/gif-icon-dark.png" alt="Ícone meus Gifs">
+            <p>Meus Gifs</p>
+        `;
+
+        iconsGifAndThemes[1].innerHTML = `
+        <img class="icons-header" src="/assets/img/icons/theme-dark.png" alt="Ícone de tema escuro">
+        <p>Dark Mode</p>
+        `
+
+        for (let i = 0; i < iconsGifAndThemes.length; i++) {
+            iconsGifAndThemes[i].style.color = '#0B0B0B'
+            iconsGifAndThemes[i].style.transition = '0.5s'
+        }
+
+        infosMiku.style.color = '#0B0B0B';
+        infosMiku.style.transition = '0.5s';
+
+        question.classList.remove('answers-questions-black');
+
+        for (let i = 0; i < answers.length; i++) {
+            answers[i].classList.remove('answers-questions-black');
+        }
+
+        if (document.getElementById('in-system-show-gifs')) {
+            document.getElementById('in-system-show-gifs').classList.toggle('answers-questions-black');
+        }
+      
+        controlsDarkTheme = false;
+
+        localStorage.setItem('themeDark', false);
+    }
+
+}
+
+buttonDarkTheme.addEventListener('click', toPlaceThemeDark);
---
+++ src/js/script.js
@@ -0,0 +1,132 @@
+// Selecionar elementos
+const getAnswers = document.getElementsByClassName('answers');
+const getBody = document.querySelector('body');
+const getMain = document.querySelector('main');
+const myGifs = document.querySelector('header div');
+
+let myLinkGifs = [];
+
+const storedGifs = JSON.parse(localStorage.getItem('gifs'));
+if (storedGifs) {
+    myLinkGifs = storedGifs;
+}
+
+for (const i of getAnswers) {
+    i.addEventListener('click', async () => {
+        let contentAnswers;
+
+        switch (i.textContent) {
+            case 'Feliz':
+                contentAnswers = 'happy';
+                break;
+            case 'Triste':
+                contentAnswers = 'sad';
+                break;
+            case 'Apaixonado':
+                contentAnswers = 'love';
+                break;
+            case 'Nervoso':
+                contentAnswers = 'scared';
+                break;
+            case 'Sono':
+                contentAnswers = 'sleep';
+                break;
+            case 'Raiva':
+                contentAnswers = 'angrystare';
+                break;
+            default:
+                contentAnswers = '';
+                break;
+        }
+
+        if (!contentAnswers) return;
+
+        try {
+            const response = await fetch(`https://api.otakugifs.xyz/gif?reaction=${contentAnswers}`);
+            const responseJson = await response.json();
+
+            if (response.status === 200 && responseJson.url) {
+                myLinkGifs.push(responseJson);
+                localStorage.setItem('gifs', JSON.stringify(myLinkGifs));
+
+                getMain.innerHTML = `
+                    <div id="show-gif">
+                        <img id="miku-show-gif" src="./assets/img/miku/miku-person-3.png">
+                        <div>
+                            <img id="gif" src="${responseJson.url}">
+                            <div id="in-system-show-gifs">Meus Gifs</div>
+                        </div>
+                    </div>
+                `;
+
+                if (document.getElementById('dark-theme').textContent.trim() =='Light Mode') {
+                    document.getElementById('in-system-show-gifs').classList.add('answers-questions-black');
+                }
+                
+            } else {
+                getBody.innerHTML = `
+                    <section id="error-body">
+                        <h1>Algo deu errado...</h1>
+                        <img src="./assets/img/miku/miku-person-5.png">
+                    </section>
+                `;
+            }
+
+            const secondMyGifs = document.getElementById('in-system-show-gifs');
+
+            secondMyGifs.addEventListener('click', ()=>{
+            getMain.innerHTML = `
+            <section id="my-gifs-section">
+                <img src="./assets/img/miku/miku-person-4.png">
+                <div id="div-d-my-gifs">
+                    <h2>Meus Gifs</h2>
+                    <div id="div-gifs"></div>
+                </div>
+            </section>
+            `;
+
+            const divPai = document.getElementById('div-gifs');
+
+            if (myLinkGifs.length) {
+                for (const gifObj of myLinkGifs) {
+                    const gif = document.createElement('img');
+                    gif.setAttribute('src', gifObj.url);
+                    divPai.appendChild(gif);
+                }
+            } else {
+                divPai.innerHTML = `<p id="div-gif-vazio">Não há gifs armazenados.</p>`;
+            }
+            })
+        } catch (err) {
+            console.log(`Erro: ${err}`);
+        }
+    });
+}
+
+myGifs.addEventListener('click', () => {
+    getMain.innerHTML = `
+        <section id="my-gifs-section">
+            <img src="./assets/img/miku/miku-person-4.png">
+            <div id="div-d-my-gifs">
+                <h2>Meus Gifs</h2>
+                <div id="div-gifs"></div>
+            </div>
+        </section>
+    `;
+
+    const divPai = document.getElementById('div-gifs');
+
+    if (myLinkGifs.length) {
+        for (const gifObj of myLinkGifs) {
+            const gif = document.createElement('img');
+            gif.setAttribute('src', gifObj.url);
+            divPai.appendChild(gif);
+        }
+    } else {
+        divPai.innerHTML = `<p id="div-gif-vazio">Não há gifs armazenados.</p>`;
+    }
+});
+
+
+
+
